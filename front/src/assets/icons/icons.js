35589a48c634d8f82aa8d3739cc8f2d947056aae
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2023-01-13 09:33:19 +0100

    added icons to assest folder

---
+++ front/src/assets/icons/icons.js
@@ -0,0 +1,123 @@
+export const logoIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 24 24'
+		fill='currentColor'
+		className='w-6 h-6 mr-3'
+	>
+		<path d='M11.584 2.376a.75.75 0 01.832 0l9 6a.75.75 0 11-.832 1.248L12 3.901 3.416 9.624a.75.75 0 01-.832-1.248l9-6z' />
+		<path
+			fillRule='evenodd'
+			d='M20.25 10.332v9.918H21a.75.75 0 010 1.5H3a.75.75 0 010-1.5h.75v-9.918a.75.75 0 01.634-.74A49.109 49.109 0 0112 9c2.59 0 5.134.202 7.616.592a.75.75 0 01.634.74zm-7.5 2.418a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75zm3-.75a.75.75 0 01.75.75v6.75a.75.75 0 01-1.5 0v-6.75a.75.75 0 01.75-.75zM9 12.75a.75.75 0 00-1.5 0v6.75a.75.75 0 001.5 0v-6.75z'
+			clipRule='evenodd'
+		/>
+		<path d='M12 7.875a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z' />
+	</svg>
+);
+
+export const userIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='w-5 h-5 mr-2'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+		/>
+	</svg>
+);
+
+export const homeIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='w-5 h-5 mr-3'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+		/>
+	</svg>
+);
+
+export const transactionsIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='w-5 h-5 mr-3'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5'
+		/>
+	</svg>
+);
+
+export const accountsIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='w-5 h-5 mr-3'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z'
+		/>
+	</svg>
+);
+
+export const addIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='w-5 h-5 mr-3'
+	>
+		<path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+	</svg>
+);
+
+export const addIconWithBorder = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={2}
+		stroke='currentColor'
+		className='w-6 h-6 border border-more-gray rounded p-0.5 hover:bg-dark-blue hover:text-white'
+	>
+		<path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
+	</svg>
+);
+
+export const closeIcon = (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={1.5}
+		stroke='currentColor'
+		className='w-5 h-5'
+	>
+		<path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+	</svg>
+);
--- front/src/components/Users/Form.jsx
+++ front/src/components/Users/Form.jsx
@@ -1,29 +1,57 @@
+import Button from '../UI/Button';
+
 export default function Form({ formType, onChange, onSubmit }) {
 	return (
-		<form onSubmit={onSubmit}>
-			<h2>{formType}</h2>
-			{formType === 'Signup' && (
-				<div>
-					<label htmlFor='name'>User name:</label>
-					<input onChange={onChange} type='text' id='name' name='name' placeholder='User name' />
+		<div className='w-full max-w-xs m-auto mt-6'>
+			<h4 className='text-xl mb-2'>{formType}</h4>
+			<form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4' onSubmit={onSubmit}>
+				{formType === 'Signup' && (
+					<div className='mb-4'>
+						<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
+							User name:
+						</label>
+						<input
+							className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+							onChange={onChange}
+							type='text'
+							id='name'
+							name='name'
+							placeholder='User name'
+						/>
+					</div>
+				)}
+				<div className='mb-6'>
+					<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
+						Email:
+					</label>
+					<input
+						className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+						onChange={onChange}
+						type='text'
+						id='email'
+						name='email'
+						placeholder='Email'
+					/>
 				</div>
-			)}
-			<div>
-				<label htmlFor='email'>Email:</label>
-				<input onChange={onChange} type='text' id='email' name='email' placeholder='Email' />
-			</div>
-
-			<div>
-				<label htmlFor='password'>Password:</label>
-				<input
-					onChange={onChange}
-					type='password'
-					id='password'
-					name='password'
-					placeholder='Password'
-				/>
-			</div>
-			<button>{formType}</button>
-		</form>
+				<div>
+					<label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password'>
+						Password:
+					</label>
+					<input
+						className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+						onChange={onChange}
+						type='password'
+						id='password'
+						name='password'
+						placeholder='Password'
+					/>
+				</div>
+				<div className='flex items-center justify-center'>
+					<Button type='submit' name={formType === 'Signup' ? 'Signup' : 'Login'}>
+						{formType}
+					</Button>
+				</div>
+			</form>
+		</div>
 	);
 }
